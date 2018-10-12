eefe9fd1cf8523e854a1faa1f82d764b4aecbc5c
Hawkfrost000 <43019110+Hawkfrost000@users.noreply.github.com>
2018-10-12 10:55:37 -0700

    Add files via upload
    the most firstest of drafts

---
+++ Minesweeper.hs
@@ -0,0 +1,53 @@
+module Minesweeper where
+
+-- To run it, try:
+-- ghci
+-- :load Minesweeper
+
+data State = State [Action]  -- internal_state available_actions
+         deriving (Ord, Eq)
+
+data Result = EndOfGame Double State    -- end of game, value, starting state
+            | ContinueGame State        -- continue with new state
+         deriving (Eq)
+
+type Game = Action -> State -> Result
+
+type Player = State -> Action
+
+-----------------Minesweeper Game --------------------------------------
+
+newtype Action = Action (Int, Int)
+	deriving (Eq, Ord)
+	
+-- Game board Enumeration
+-- 0 - empty
+-- 1 - empty, flagged
+-- 2 - empty, cleared
+-- 3 - bomb
+-- 4 - bomb, flagged
+
+-- the game is over when all the B's are replaced with F's and there are
+-- no more 1's
+
+empty = 0
+emptyFlagged = 1
+emptyCleared = 2
+bomb = 3
+bombFLagged = 4
+
+small_grid = [[0,0,0], [0,3,0],[0,0,0]]
+
+-- note: find_replace uses 1 indexing and (1,1) is the top left corner of the grid
+
+find_replace :: [[t]] -> Int -> Int -> t -> [[t]]
+find_replace [] x y c = []
+find_replace (first:rest) x y c
+	|y == 1 = (find_replace_helper first x c) : rest
+	|otherwise = first : find_replace rest x (y-1) c
+	
+find_replace_helper :: [t] -> Int -> t -> [t]
+find_replace_helper [] x c = []
+find_replace_helper (first:rest) x c
+	|x == 1 = c : rest
+	|otherwise = first : (find_replace_helper rest (x-1) c)
