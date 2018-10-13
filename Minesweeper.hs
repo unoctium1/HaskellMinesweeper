dd276df24d08129e7cca5e237bda120ceba00813
Hawkfrost000 <43019110+Hawkfrost000@users.noreply.github.com>
2018-10-12 17:42:01 -0700

    Update Minesweeper.hs

--- Minesweeper.hs
+++ Minesweeper.hs
@@ -16,9 +16,12 @@ type Game = Action -> State -> Result
 type Player = State -> Action
 
 -----------------Minesweeper Game --------------------------------------
-
+-- am action is a triple of an x value, a y value, and the value to be placed
+-- at said x,y location
 newtype Action = Action (Int, Int, Int)
 	deriving (Eq, Ord)
+	
+-- an internal state is the minesweeper grid as described below
 type InternalState = [[Int]]
 	
 -- Game board Enumeration
@@ -28,18 +31,24 @@ type InternalState = [[Int]]
 -- 3 - bomb
 -- 4 - bomb, flagged
 
--- the game is over when all the B's are replaced with F's and there are
+-- the game is over when all the 3's are replaced with 4's and there are
 -- no more 1's
 
-empty = 0
-emptyFlagged = 1
-emptyCleared = 2
-bomb = 3
-bombFLagged = 4
+empty = 0				-- empty space, uncleared
+emptyFlagged = 1		-- empty space, flagged incorrectly
+emptyCleared = 2		-- empty space, cleared over the course of the game
+mine = 3				-- mine, undiscovered
+bombFLagged = 4			-- mine, flagged
+
+-- a small grid for testing purposes, feel free to design your own
 
 small_grid = [[0,0,0], [0,3,0],[0,0,0]]
 
 -- note: find_replace uses 1 indexing and (1,1) is the top left corner of the grid
+-- if 1 indexing proves really inconvenient we can reformat
+
+-- find_replace takes a grid, an x,y coordinate, and the number to replace
+-- the number located at said coordinate. It returns the updated grid
 
 find_replace :: [[t]] -> Int -> Int -> t -> [[t]]
 find_replace [] x y c = []
@@ -53,12 +62,18 @@ find_replace_helper (first:rest) x c
 	|x == 1 = c : rest
 	|otherwise = first : (find_replace_helper rest (x-1) c)
 	
-minesweeper_start = State small_grid
+minesweeper_start = State small_grid		-- initializes the game with the test grid
 
 minesweeper :: Game
 minesweeper (Action (x,y,c)) (State (grid))
-	| win (find_replace grid x y c)	= EndOfGame 1 minesweeper_start
-	|otherwise 			  			= ContinueGame (State(find_replace grid x y c))
+	| win (find_replace grid x y c)	= EndOfGame 1 minesweeper_start						-- did we win?
+	|otherwise 			  			= ContinueGame (State(find_replace grid x y c))		-- if not the game goes on
+
+--TODO: Flesh out the win and loss conditions for the game, for example
+-- we should loose if we click on a mine or run out of moves/time
+
+-- we win if all the 3's are gone
+-- note that this win condition assumes we cannot incorrectly remove 3's
 
 win :: [[Int]] -> Bool
 win [] = True
